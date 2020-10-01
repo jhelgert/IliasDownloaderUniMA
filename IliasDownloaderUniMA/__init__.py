76c47f4824b30c76b46923c35a4484a6ba54a5ff
Jonas Veit <26003949+jonas7654@users.noreply.github.com>
2020-10-01 21:48:25 +0200

    fix for date out of range error (#9)

--- IliasDownloaderUniMA/__init__.py
+++ IliasDownloaderUniMA/__init__.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 from pathlib import Path as plPath
 from dateutil.parser import parse as parsedate
-from datetime import datetime
+from datetime import datetime, timedelta
 from multiprocessing.pool import ThreadPool
 import math
 import os	
@@ -137,7 +137,7 @@ class IliasDownloaderUniMA():
 		"""
 
 		today = datetime.now()
-		gestern = today.replace(day = today.day-1).strftime("%d. %b %Y")
+	gestern = (today - timedelta(1)).strftime("%d. %b %Y")		
 		d = {"Mär": "Mar", "Mai": "May", "Jun": "Jun", "Jul": "Jul", "Okt": "Oct", "Dez": "Dec",
 		"Gestern": gestern, 'yesterday': gestern, "Heute": today.strftime("%d. %b %Y"), "today": today.strftime("%d. %b %Y")}
 		for key in d.keys():

4b3120ec4d53b5233ca6492e77c65476b1b2db4a
jhelgert <jonathan.helgert@posteo.de>
2020-10-11 17:59:27 +0200

    Set required python version to 3.7

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as f:
 
 setup(
 	name="IliasDownloaderUniMA",
-	version='0.4.1',
+	version='0.4.2',
 	packages=['IliasDownloaderUniMA'],
 	author='Jonathan Helgert',
 	author_email='jhelgert@mail.uni-mannheim.de',
 	description='A simple package to download files from ilias.uni-mannheim.de',
 	long_description=long_description,
 	long_description_content_type="text/markdown",
-	python_requires='>=3.5',
+	python_requires='>=3.7',
 	classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",

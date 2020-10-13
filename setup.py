1f4f5f99defd5da9ff0065020365689775b9aed3
jhelgert <jonathan.helgert@posteo.de>
2020-10-13 09:52:56 +0200

    Added verbose option and fixed parsing of file properties

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
 	name="IliasDownloaderUniMA",
-	version='0.4.2',
+	version='0.4.3',
 	packages=['IliasDownloaderUniMA'],
 	author='Jonathan Helgert',
 	author_email='jhelgert@mail.uni-mannheim.de',

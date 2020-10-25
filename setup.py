aa28d18ea89b8d9e0bc23fa737d6e518f6d37082
jhelgert <jonathan.helgert@posteo.de>
2020-10-25 17:14:31 +0100

    New Version + New requirements: Python3.8 and dateparser library

--- setup.py
+++ setup.py
@@ -5,20 +5,21 @@ with open("README.md", "r") as f:
 
 setup(
 	name="IliasDownloaderUniMA",
-	version='0.4.3',
+	version='0.4.5',
 	packages=['IliasDownloaderUniMA'],
 	author='Jonathan Helgert',
 	author_email='jhelgert@mail.uni-mannheim.de',
 	description='A simple package to download files from ilias.uni-mannheim.de',
 	long_description=long_description,
 	long_description_content_type="text/markdown",
-	python_requires='>=3.7',
+	python_requires='>=3.8',
 	classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
 	install_requires=[
+		"dateparser",
 		"bs4",
 		"requests",
 		"python-dateutil",

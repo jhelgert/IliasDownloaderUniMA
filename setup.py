e2cf1c4ba9c96e3956deb386f1b1691c5ca3dcf2
Jonathan Helgert <jhelgert@users.noreply.github.com>
2020-11-24 22:58:49 +0100

    Update setup.py
    Added project url (for pypi).

--- setup.py
+++ setup.py
@@ -4,21 +4,22 @@ with open("README.md", "r") as f:
 	long_description = f.read()
 
 setup(
-	name="IliasDownloaderUniMA",
-	version='0.5.0',
-	packages=['IliasDownloaderUniMA'],
-	author='Jonathan Helgert',
-	author_email='jhelgert@mail.uni-mannheim.de',
-	description='A simple package to download files from ilias.uni-mannheim.de',
-	long_description=long_description,
-	long_description_content_type="text/markdown",
-	python_requires='>=3.8',
-	classifiers=[
+	name = "IliasDownloaderUniMA",
+	url = "https://github.com/jhelgert/IliasDownloaderUniMA",
+	version = '0.5.0',
+	packages = ['IliasDownloaderUniMA'],
+	author = 'Jonathan Helgert',
+	author_email = 'jhelgert@mail.uni-mannheim.de',
+	description = 'A simple package to download files from ilias.uni-mannheim.de',
+	long_description = long_description,
+	long_description_content_type = "text/markdown",
+	python_requires = '>=3.8',
+	classifiers = [
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-	install_requires=[
+	install_requires = [
 		"dateparser",
 		"bs4",
 		"requests",
